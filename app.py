b2756ed6a31a5025297d688f43bd404c881d4d36
ithihas <f20180607@goa.bits-pilani.ac.in>
2020-10-22 21:31:32 +0530

    Added resubmit option

--- app.py
+++ app.py
@@ -7,15 +7,25 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+input_text = ''
+
 @app.route('/your-disease', methods=['POST'])
 def home_post():
-    text = request.form.get('symptoms_input')
-    words = word_extractor(text)
-    final_symptoms = symptoms(words)
-    print(final_symptoms)
-    disease = predictor(final_symptoms)[0]
-    return render_template('disease.html', text = text, final_symptoms = final_symptoms, disease = disease)
+    if request.method == 'POST':
+        text = request.form.get('symptoms_input')
+        words = word_extractor(text)
+        final_symptoms = symptoms(words)
+        disease = predictor(final_symptoms)[0]
+        global input_text
+        input_text = text
+        return render_template('disease.html', text = text, final_symptoms = final_symptoms, disease = disease)
 
+@app.route('/your-disease-re', methods=['POST'])
+def home_repost():
+    if request.method == 'POST':
+        final_symptoms = request.form.getlist('disease_checkbox')
+        disease = predictor(final_symptoms)[0]
+        return render_template('disease.html', text = input_text, final_symptoms = final_symptoms, disease = disease)
 
 if __name__ == '__main__':
     app.run(debug=True)
