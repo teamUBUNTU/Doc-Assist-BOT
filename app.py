af7683747e15890017e0ad39a40b25d9842afc40
ithihas <f20180607@goa.bits-pilani.ac.in>
2020-10-24 12:47:48 +0530

    Added database functionality

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request
 from preprocess import word_extractor, symptoms
 from predict import predictor
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyAtb_ClJ1maEUb20NZ3lNmPZJDMoX5M2mw",
+    "authDomain": "ubuntu-doc-assist.firebaseapp.com",
+    "databaseURL": "https://ubuntu-doc-assist.firebaseio.com",
+    "projectId": "ubuntu-doc-assist",
+    "storageBucket": "ubuntu-doc-assist.appspot.com",
+    "messagingSenderId": "325752264622",
+    "appId": "1:325752264622:web:255050e2424ad547035a3f",
+    "measurementId": "G-65JHR5Q2B5"  
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,14 +34,24 @@ def home_post():
         diseases = predictor(final_symptoms)
         global input_text
         input_text = text
-        return render_template('disease.html', text = text, final_symptoms = final_symptoms, diseases = diseases)
+        
+        if request.form.get('accept'):
+            db.child('Accepted Diseases').child(input_text).update({"Symptoms": final_symptoms})
+            db.child('Accepted Diseases').child(input_text).update({"Disease": diseases})
+        
+    return render_template('disease.html', text = text, final_symptoms = final_symptoms, diseases = diseases)
 
 @app.route('/your-disease-re', methods=['POST'])
 def home_repost():
     if request.method == 'POST':
         final_symptoms = request.form.getlist('disease_checkbox')
         diseases = predictor(final_symptoms)
-        return render_template('disease.html', text = input_text, final_symptoms = final_symptoms, diseases = diseases)
+        
+        if request.form.get('accept'):
+            db.child('Accepted Diseases').child(input_text).update({"Symptoms": final_symptoms})
+            db.child('Accepted Diseases').child(input_text).update({"Disease": diseases})
+            
+    return render_template('disease.html', text = input_text, final_symptoms = final_symptoms, diseases = diseases)
 
 if __name__ == '__main__':
     app.run(debug=True)
